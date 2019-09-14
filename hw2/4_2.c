d2ba46bd3d2fbf504581495d8404e8bcefdf323c
Jihun Kim <starlettkim@gmail.com>
2019-09-15 01:00:33 +0900

    Small fix on problem 4.2

--- hw2/4_2.c
+++ hw2/4_2.c
@@ -3,9 +3,8 @@
 
 void solve_4_2() {
     printf("========== Problem 4.2 ==========\n");
-    const int MAX_STEP = 15;
     double sum = 1, term = 1;
-    for (int i = 1; i <= MAX_STEP; i++) {
+    for (int i = 1; sum != .5; i++) {
         term *= -M_PI * M_PI / ((2 * i - 1) * (2 * i)) / 9;
         sum += term;
         printf("Step #%d: estimated %e, relative err %f%%\n", i, sum, fabs(.5 - sum) * 100 / sum);
