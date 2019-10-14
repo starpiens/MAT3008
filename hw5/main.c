6e6f6e9197463cd90f1a400c740ba57256eb2f28
Jihun Kim <starlettkim@gmail.com>
2019-10-14 17:06:45 +0900

    Implement load_data

--- hw5/main.c
+++ hw5/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
 float *run_gaussj(int N, float **A, float *b) {
 
@@ -20,7 +22,20 @@ float run_svdcmp(int N, float **A, float *b) {
  * @param b 1D array representing vector b.
  */
 void load_data(FILE *ifp, int *N, float **A, float *b) {
-
+    int M;
+    fscanf(ifp, "%d %d", &M, N);
+    assert(M == *N);
+    // Make space for A and b.
+    A = calloc(*N + 1, sizeof(float *));
+    for (int i = 1; i <= *N; i++)
+        A[i] = calloc(*N + 1, sizeof(float));
+    b = calloc(*N + 1, sizeof(float));
+    // Read A, b.
+    for (int i = 1; i <= *N; i++)
+        for (int j = 1; j <= *N; j++)
+            fscanf(ifp, "%f", &A[i][j]);
+    for (int i = 1; i <= *N; i++)
+        fscanf(ifp, "%f", &b[i]);
 }
 
 /**
@@ -31,17 +46,24 @@ void load_data(FILE *ifp, int *N, float **A, float *b) {
  */
 int main(int argc, char *argv[]) {
     FILE *ifp;
+    int N;
+    float **A = NULL;
+    float *b = NULL;
+
     ifp = fopen(argv[1], "r");
     if (ifp == NULL) {
         fprintf(stderr, "Failed to open file: %s\n", argv[1]);
     }
-    int N;
-    float **A;
-    float *b;
     load_data(ifp, &N, A, b);
+    fclose(ifp);
+
     run_gaussj(N, A, b);
     run_ludcmp(N, A, b);
     run_svdcmp(N, A, b);
 
+    for (int i = 1; i <= N; i++)
+        free(A[i]);
+    free(A);
+    free(b);
     return 0;
 }
