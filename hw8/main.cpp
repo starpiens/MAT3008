886a4e2921137bcdedeba22a3ee79b29fa5cad7b
Jihun Kim <starlettkim@gmail.com>
2019-11-18 23:29:45 +0900

    Finished hw8

--- hw8/main.cpp
+++ hw8/main.cpp
@@ -1,25 +1,68 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <opencv/cv.hpp>
 
-int main() {
-    cv::Mat before = cv::imread("../img/Lenna.png", CV_LOAD_IMAGE_COLOR);
-    if (!before.data) {
-        std::cerr << "Error while reading image" << std::endl;
-        return -1;
-    }
-    cv::imshow("before", before);
-    cv::waitKey(0);
+template <typename _Tp>
+void resize_bilinear(const cv::Mat &org_img, cv::Mat &new_img) {
+    double x_rate = (double)new_img.cols / org_img.cols;
+    double y_rate = (double)new_img.rows / org_img.rows;
 
-    cv::Mat after(cv::Size(before.rows, before.cols), CV_8UC3);
-    for (int y = 0; y < before.rows; y++) {
-        for (int x = 0; x < before.cols; x++) {
-            after.at<cv::Vec3b>(y, x)[0] = before.at<cv::Vec3b>(y, x)[2];
-            after.at<cv::Vec3b>(y, x)[1] = before.at<cv::Vec3b>(y, x)[1];
-            after.at<cv::Vec3b>(y, x)[2] = before.at<cv::Vec3b>(y, x)[0];
+    for (int x = 0; x < new_img.cols; x++) {
+        for (int y = 0; y < new_img.rows; y++) {
+            int x_org = (int)(x / x_rate);
+            int y_org = (int)(y / y_rate);
+
+            double fx1 = (double)x / x_rate - x_org;
+            double fx2 = 1 - fx1;
+            double fy1 = (double)y / y_rate - y_org;
+            double fy2 = 1 - fy1;
+
+            double w1 = fx2 * fy2;
+            double w2 = fx1 * fy2;
+            double w3 = fx2 * fy1;
+            double w4 = fx1 * fy1;
+
+            _Tp P1 = org_img.at<_Tp>(y_org, x_org);
+            _Tp P2 = org_img.at<_Tp>(y_org, x_org + 1);
+            _Tp P3 = org_img.at<_Tp>(y_org + 1, x_org);
+            _Tp P4 = org_img.at<_Tp>(y_org + 1, x_org + 1);
+
+            new_img.at<_Tp>(y, x) = w1 * P1 + w2 * P2 + w3 * P3 + w4 * P4;
         }
     }
+}
 
-    cv::imshow("after", after);
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0]
+            << " <filename>"
+            << " <size_x>"
+            << " <size_y>"
+            << std::endl;
+        exit(-1);
+    }
+    std::string filename(argv[1]);
+    int after_x = std::stoi(argv[2]);
+    int after_y = std::stoi(argv[3]);
+
+    cv::Mat before;
+    before = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+    if (!before.data) {
+        std::cerr << "Error while reading image" << std::endl;
+        exit(-1);
+    }
+    std::stringstream winname;
+    winname << "Before (" << before.cols << "x" << before.rows << ")\n";
+    cv::imshow(winname.str(), before);
+    cv::waitKey(0);
+
+    cv::Mat after(after_y, after_x, CV_8UC3);
+    resize_bilinear<cv::Vec3b>(before, after);
+
+    winname.str("");
+    winname << "After (" << after.cols << "x" << after.rows << ")\n";
+    cv::imshow(winname.str(), after);
     cv::waitKey(0);
 
     return 0;
