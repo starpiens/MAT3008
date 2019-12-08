71a61941ea9d3bdd1e76e791ca51159253a674d6
Jihun Kim <starlettkim@gmail.com>
2019-12-08 17:55:20 +0900

    Implement get_coreespondences()

--- hw10/main.cpp
+++ hw10/main.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/features2d.hpp"
 
+using namespace std;
+using namespace cv;
+
+vector<pair<Point2f, Point2f>> get_correspondences(const Mat &left, const Mat &right, Mat *corr_img = nullptr) {
+    const int DIST_THRESH = 30;
+
+    Ptr<Feature2D> feature_extractor;
+    feature_extractor = ORB::create();
+    vector<KeyPoint> left_keypoints, right_keypoints;
+    Mat left_descriptors, right_descriptors;
+    feature_extractor->detectAndCompute(left, Mat(), left_keypoints, left_descriptors);
+    feature_extractor->detectAndCompute(right, Mat(), right_keypoints, right_descriptors);
+
+    Ptr<DescriptorMatcher> matcher;
+    vector<DMatch> matches;
+    matcher = DescriptorMatcher::create("BruteForce-Hamming");
+    matcher->match(left_descriptors, right_descriptors, matches);
+    matches.end() = remove_if(matches.begin(), matches.end(), [](DMatch i) -> bool {
+      return i.distance > DIST_THRESH;
+    });
+    if (corr_img != nullptr) {
+        drawMatches(left, left_keypoints, right, right_keypoints, matches, *corr_img);
+    }
+
+    vector<pair<Point2f, Point2f>> ret;
+    ret.reserve(matches.size());
+    for (auto i : matches) {
+        ret.emplace_back(left_keypoints[i.queryIdx].pt, right_keypoints[i.trainIdx].pt);
+    }
+    return ret;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0]
-                  << " <LeftImg>"
-                  << " <RightImg>"
-                  << std::endl;
+        cerr << "Usage: " << argv[0]
+             << " <LeftImg>"
+             << " <RightImg>"
+             << endl;
         exit(-1);
     }
-    cv::Mat leftImg = cv::imread(argv[1]);
-    cv::Mat rightImg = cv::imread(argv[2]);
+    Mat left_img = imread(argv[1]);
+    Mat right_img = imread(argv[2]);
+    Mat corr_img;
 
-    cv::imshow("Left", leftImg);
-    cv::waitKey(0);
-    cv::imshow("Right", rightImg);
-    cv::waitKey(0);
+    auto correspondences = get_correspondences(left_img, right_img, &corr_img);
+    imshow("matches", corr_img);
+    waitKey(0);
 
     return 0;
 }
